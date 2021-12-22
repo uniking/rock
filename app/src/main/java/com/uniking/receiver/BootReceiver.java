4178ab1d32e3321586553c1d952a95ff4e50f943
wangzhl <578246786@qq.com>
2021-12-22 16:51:07 +0800

    开机自启动

---
+++ app/src/main/java/com/uniking/receiver/BootReceiver.java
@@ -0,0 +1,16 @@
+package com.uniking.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by wzl on 11/29/21.
+ */
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
